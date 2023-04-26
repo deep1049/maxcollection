766fecb337278d580628ada00bcaa4823e82f8d9
deep1049 <103635801+deep1049@users.noreply.github.com>
2023-04-26 17:38:21 +0530

    react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import AllRoutes from "./Components/allRoutes";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AllRoutes />
     </div>
   );
 }
