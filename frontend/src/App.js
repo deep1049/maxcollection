026478b388226a48093a3694dc6a1eb5f4908a71
deep1049 <103635801+deep1049@users.noreply.github.com>
2023-04-28 19:38:08 +0530

    react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,10 @@ import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Navbar />
     </div>
   );
 }
